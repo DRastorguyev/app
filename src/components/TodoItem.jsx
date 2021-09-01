3a5eb93f3fbddd1ef1908d73357ed2e01b694042
daniil <daniil.alpaca@gmail.com>
2021-09-01 19:03:24 +0300

    add confirm button

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -5,18 +5,18 @@ const TodoItem = (props) => {
   return (
     <div>
       <div
-        onClick={() => props.selectToDo(props.todo.id)}
+        
         className={props.todo.done ? ["todo", "todoDone"].join(" ") : "todo"}
       >
         <div className="todo__content">
           <div>
-            {props.number}. {props.todo.title}
+              <MyButton onClick={() => props.selectToDo(props.todo.id)}><i class="far fa-check-circle"></i></MyButton>  {props.number}. {props.todo.title}
           </div>
         </div>
         <div className="todo__btns">
           {props.todo.date.substr(0, 10)}
           <MyButton
-            style={{ marginLeft: 10 }}
+            style={{marginLeft: 25}}
             onClick={(e) => {
               e.stopPropagation();
               props.remove(props.todo);
--- src/components/UI/button/MyButton.jsx
+++ src/components/UI/button/MyButton.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import cl from "../button/MyButton.module.css";
 
-const MyButton = ({ children, current, onClick }) => {
+const MyButton = ({ children, current, onClick, style }) => {
   return (
-    <button 
+    <button
+      style={style}
       onClick={onClick} 
       className={current ? [cl.myBtn, cl.activePaginateButton].join(' ') : cl.myBtn}
     >
