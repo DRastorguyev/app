a2e95b3ba0eba6460eb4a618fa3087328f36af40
daniil <daniil.alpaca@gmail.com>
2021-09-24 11:45:23 +0300

    change axios URL for Heroku

--- src/http/index.js
+++ src/http/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const $host = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://drastorguyev.github.io/toDo-frontend/',
 });
 
 const $authHost = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://drastorguyev.github.io/toDo-frontend/',
 });
 
 const authInterceptor = (config) => {
